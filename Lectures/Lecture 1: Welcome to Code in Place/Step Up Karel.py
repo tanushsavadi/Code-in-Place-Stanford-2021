5271350a14efcfce8af90bbf12475ea895b7a833
Tanush <82804813+CodingForTheBetter@users.noreply.github.com>
2021-07-13 21:25:10 +0400

    Create Step Up Karel.py

---
+++ Lectures/Lecture 1: Welcome to Code in Place/Step Up Karel.py
@@ -0,0 +1,30 @@
+from karel.stanfordkarel import *
+
+"""
+File: StepUp.py
+--------------------
+Karel should pick up the beeper and put it on the ledge
+"""
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    pass
+
+if __name__ == "__main__":
+    run_karel_program('StepUp.w')
