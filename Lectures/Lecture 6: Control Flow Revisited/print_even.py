ff9e9f24422178be0032e558c04e46f6b0133c0a
Tanush <82804813+CodingForTheBetter@users.noreply.github.com>
2021-10-11 14:14:14 +0400

    Create print_even.py

---
+++ Lectures/Lecture 6: Control Flow Revisited/print_even.py
@@ -0,0 +1,14 @@
+"""
+Print Even Numbers
+Write a program to print the first 100 even numbers
+
+Example of using the index variable of a for loop
+"""
+
+def main():
+    for i in range(1,101):
+        if i % 2 == 0:
+            print(i, "Even number")
+
+if __name__ == '__main__':
+    main()
