d19dfbe92c84b7218f0b653da0357ec77ccac960
Tanush <82804813+CodingForTheBetter@users.noreply.github.com>
2021-10-11 13:00:42 +0400

    Create add2numbers.py

---
+++ Lectures/Lecture 4: Introduction to Python/add2numbers.py
@@ -0,0 +1,35 @@
+"""
+Another python program to get some practice with variables.
+This program asks the user for two integers and prints their sum.
+"""
+
+def main():
+    print("This program adds two numbers.")
+    num1 = input("Enter first number: ")
+    num1 = int(num1)
+    num2 = input("Enter second number: ")
+    num2 = int(num2)
+    total = num1 + num2
+    print("The total is " + str(total) + ".")
+
+#This provided line is required at the end of a Python file
+#to call the main() function.
+if __name__ == '__main__':
+    main()
+  
+
+  
+
+#ALTERNATE SOLUTION:
+
+#def main():
+#    print("This program adds two numbers.")
+#    num1 = int(input("Enter first number: "))
+#    num2 = int(input("Enter second number: "))
+#    total = num1 + num2
+#    print("The total is " + str(total) + ".")
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+#if __name__ == '__main__':
+#    main()
