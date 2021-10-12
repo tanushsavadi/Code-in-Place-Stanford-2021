27008a5c72f2742daac521c6fbf2de2fba8586f7
Tanush <82804813+CodingForTheBetter@users.noreply.github.com>
2021-10-12 11:48:33 +0400

    Create factorial.py

---
+++ Lectures/Lecture 8: Core Complete/factorial.py
@@ -0,0 +1,30 @@
+# Constant – visible to all functions
+MAX_NUM = int(input("Enter the number you want the factorial of: "))
+
+def main():
+    # repeat for several values!
+    for i in range(MAX_NUM+1):
+        print(i, factorial(i))
+    
+def factorial(n):
+    """
+    Calculates n factorial.
+    5 factorial is 5 * 4 * 3 * 2 * 1
+    >>> factorial(5)
+    120
+    >>> factorial(4)
+    24
+    >>> factorial(3)
+    6
+    >>> factorial(1)
+    1
+    >>> factorial(0)
+    1
+    """
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+    
+if __name__ == '__main__':
+    main()
